a9c229d52ad53796b3a8220a5a2657c07883ea00
Jean-Marie Dalmasso <jdalmass7@gmail.com>
2019-02-15 08:53:59 +0100

    added projects

--- server.js
+++ server.js
@@ -21,10 +21,9 @@ app.use((req, res, next) => {
   next()
 })
 
-app.use((req, res, next) => {
-  res.render("maintenance.hbs")
-  next()
-})
+// app.use((req, res, next) => {
+//   res.render("maintenance.hbs")
+// })
 
 app.use(express.static(`${__dirname}/public`))
 
@@ -46,6 +45,13 @@ app.get("/", (req, res) => {
 app.get("/about", (req, res) => {
   res.render("about.hbs", {
     title: "About Page"
+  })
+})
+
+app.get("/projects", (req, res) => {
+  res.render("projects.hbs", {
+    title: "Projects Page",
+    message: "These are your projects"
   })
 })
 
